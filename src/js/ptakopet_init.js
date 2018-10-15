e89364f52681bddd73ea2075c12c62908ded7317
Vilém Zouhar <vilem.zouhar@gmail.com>
2018-10-15 20:48:53 +0200

    fix src loading fixes

--- src/js/ptakopet_init.js
+++ src/js/ptakopet_init.js
@@ -9,75 +9,79 @@
 // add this: to make ptakopet work on your webpage
 // <script id='ptakopet_init' src='path/to/ptakopet/js/ptakopet_init.js'></script>
 
+// Ptakopět already loaded from other source
+if(typeof(ptakopet) == 'undefined') {
 
-// chrome supports chrome only
-// IE supports browser only
-// Firefox & Opera support both
-var browser = browser || chrome;
-let is_extension = (typeof browser.bookmarks != "undefined");
-let getURL = is_extension ?
-    browser.runtime.getURL :
-    function(s) { 
-        // dev/ptakopet/js/ptakopet_init.js -> ptakopet_init
-        let p = document.getElementById('ptakopet_init').src.replace('ptakopet_init.js','');
-        return p+s;
-     };
+    // chrome supports chrome only
+    // IE supports browser only
+    // Firefox & Opera support both
+    var browser = browser || chrome;
+    let is_extension = (typeof browser.bookmarks != "undefined");
+    let getURL = is_extension ?
+        browser.runtime.getURL :
+        function(s) { 
+            // dev/ptakopet/js/ptakopet_init.js -> ptakopet_init
+            let p = document.getElementById('ptakopet_init').src.replace('ptakopet_init.js','');
+            return p+s;
+        };
 
-// store current extension url in html dom, but hide it
-// easier than firing events through different contexts
-let base_url_span = document.createElement("span");
-base_url_span.id = "ptakopet_base_url_span";
-base_url_span.style.display = "none";
-base_url_span.style.fontSize = 0;
-base_url_span.innerHTML = getURL("");
-document.body.appendChild(base_url_span);
+    // store current extension url in html dom, but hide it
+    // easier than firing events through different contexts
+    let base_url_span = document.createElement("span");
+    base_url_span.id = "ptakopet_base_url_span";
+    base_url_span.style.display = "none";
+    base_url_span.style.fontSize = 0;
+    base_url_span.innerHTML = getURL("");
+    document.body.appendChild(base_url_span);
 
-// fetch floater.html content
-let floater_req = new XMLHttpRequest();
-floater_req.open("GET", getURL("../html/floater.html"));
-floater_req.addEventListener("load", 
-    function(data) {
-        if(floater_req.status != 200) 
-            alert("Something went wrong with Ptakopět (loading floater.html)");
-        else {
-            let text = floater_req.responseText;
-            document.body.innerHTML += text;
+    // fetch floater.html content
+    let floater_req = new XMLHttpRequest();
+    floater_req.open("GET", getURL("../html/floater.html"));
+    floater_req.addEventListener("load", 
+        function(data) {
+            if(floater_req.status != 200) 
+                alert("Something went wrong with Ptakopět (loading floater.html)");
+            else {
+                let text = floater_req.responseText;
+                document.body.innerHTML += text;
 
-            // add jQuery to current contex
-            let jquery = document.createElement("script");
-            jquery.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js";
-            document.body.appendChild(jquery);
+                // add jQuery to current contex
+                let jquery = document.createElement("script");
+                jquery.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js";
+                document.body.appendChild(jquery);
 
-            // scripts
-            let script_objs = document.getElementsByClassName("extension_url_script");
-            // HTMLCollection doesn't have a nice iterator
-            for(var i = 0; i < script_objs.length; i++) {
-                // spend 2 hours on this - there is no prettier way
-                let item = script_objs.item(i);
-                let item_new = document.createElement("script");
-                item_new.src = getURL(item.getAttribute("p_src"));
-                document.body.appendChild(item_new);
+                // scripts
+                let script_objs = document.getElementsByClassName("extension_url_script");
+                // HTMLCollection doesn't have a nice iterator
+                for(var i = 0; i < script_objs.length; i++) {
+                    // spend 2 hours on this - there is no prettier way
+                    let item = script_objs.item(i);
+                    let item_new = document.createElement("script");
+                    item_new.src = getURL(item.getAttribute("p_src"));
+                    document.body.appendChild(item_new);
+                }
+                for(var i = 0; i < script_objs.length; i++) {
+                    script_objs.item(i).remove();
+                }
+
+                // images
+                let img_objs = document.getElementsByClassName("extension_url_img");
+                for(var i = 0; i < img_objs.length; i++) {
+                    let item = img_objs.item(i);
+                    item.setAttribute("src", getURL(item.getAttribute("p_src")));
+                }
+
+                // styles
+                let style_objs = document.getElementsByClassName("extension_url_style");
+                for(var i = 0; i < style_objs.length; i++) {
+                    let item = style_objs.item(i);
+                    item.setAttribute("href", getURL(item.getAttribute("p_href")));
+                }
+
             }
-            for(var i = 0; i < script_objs.length; i++) {
-                script_objs.item(i).remove();
-            }
-
-            // images
-            let img_objs = document.getElementsByClassName("extension_url_img");
-            for(var i = 0; i < img_objs.length; i++) {
-                let item = img_objs.item(i);
-                item.setAttribute("src", getURL(item.getAttribute("src")));
-            }
-
-            // styles
-            let style_objs = document.getElementsByClassName("extension_url_style");
-            for(var i = 0; i < style_objs.length; i++) {
-                let item = style_objs.item(i);
-                item.setAttribute("href", getURL(item.getAttribute("href")));
-            }
-
-        }
-    });
+        });
 
 
-floater_req.send();
+    floater_req.send();
+
+}
