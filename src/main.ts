1e43b34a104aa8c0e1c896ce2aee445a9de01277
Vilém Zouhar <vilem.zouhar@gmail.com>
2019-06-21 17:29:06 +0200

    select translation backend instantiated via code

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,14 @@ let input_target = $('#input_target')
 let input_back = $('#input_back')
 let select_source = $('#language_select_source')
 let select_target = $('#language_select_target')
+let select_translator = $('#translator_backend')
+let select_qe = $('#estimator_backend')
 
 let translator_source : Translator = new TranslatorSource(input_source, input_target)
 let indicator_translator : IndicatorManager = new IndicatorManager($('#indicator_translator'))
 translator_source.addIndicator(indicator_translator)
+
+Translator.instantiateBackends(select_translator)
 
 input_source.on('input', function() {
     translator_source.translate_throttle()
@@ -19,5 +23,3 @@ input_source.on('input', function() {
 input_target.on('input', function() {
     // translator_target.translate_throttle()
 })
-
-// $('#main_content').html('<span>hello</span>' + $('#main_content').html())
--- src/translator.ts
+++ src/translator.ts
@@ -5,20 +5,37 @@ import { LanguageCode, Utils } from "./utils"
 export abstract class Translator extends AsyncMessage {
     private throttler = new Throttler(500);
 
+    /**
+     * Make a translator request, which can be later interrupted. 
+     */
     public translate_throttle() {
         this.throttler.throttle(this.translate)
     }
 
-    protected source : JQuery<HTMLElement>
-    protected target : JQuery<HTMLElement>
+    /**
+     * @param source Source textarea
+     * @param target Target textarea
+     */
     constructor(source: JQuery<HTMLElement>, target: JQuery<HTMLElement>) {
         super()
         this.source = source
         this.target = target
     }
-
+    
     protected abstract translate(): void
     protected abstract backend: TranslatorBackend
+    protected source : JQuery<HTMLElement>
+    protected target : JQuery<HTMLElement>
+    
+
+    public static instantiateBackends(selectBox: JQuery<HTMLElement>) : void {
+        $(selectBox).on('change', (a) => { console.log($(a.target).val()) })
+
+        for(let i in TranslatorBackends) {
+            $(selectBox).append($('<option>', {value: i, text: TranslatorBackends[i].name}))
+        }
+        $(selectBox).trigger('change')
+    } 
 }
 
 export class TranslatorSource extends Translator {
@@ -39,6 +56,7 @@ type TranslatorBackend = {
     composeRequest: (text: string, sourceLang: LanguageCode, targetLang: LanguageCode) => JQuery.AjaxSettings,
     languages: Array<[LanguageCode, string]>,
     sanitizeData(data: any) : string,
+    name: string,
 }
 
 let TranslatorBackends : {[index: string]: TranslatorBackend} = {
@@ -55,5 +73,23 @@ let TranslatorBackends : {[index: string]: TranslatorBackend} = {
             return data
         },
         languages: Utils.generatePairs<LanguageCode>(['cs', 'en', 'fr', 'hi'], false),
+        name: 'ÚFAL Transformer',
+    },
+
+
+    ufalTransformer2: {
+        composeRequest(text: string, sourceLang: LanguageCode, targetLang: LanguageCode): JQuery.AjaxSettings {
+            return {
+                type: "POST",
+                url: "https://lindat.mff.cuni.cz/services/transformer/api/v2/languages/",
+                data: { src: sourceLang, tgt: targetLang, input_text: text },
+                async: true,
+            }
+        },
+        sanitizeData(data: any) : string {
+            return data
+        },
+        languages: Utils.generatePairs<LanguageCode>(['cs', 'en', 'fr', 'hi'], false),
+        name: 'ÚFAL Transformer 2',
     }
 }
