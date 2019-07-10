53330df72a9de94a54c0db23afc08d254faaa7ca
Vilem Zouhar <vilem.zouhar@gmail.com>
2019-07-10 14:46:22 +0200

    QuEst++ install.sh part

--- install.sh
+++ install.sh
@@ -4,10 +4,22 @@ die() { echo "$*" 1>&2 ; exit 1; }
 # check a command is installed and in path
 assertCommand() { 
     if ! command -v $1 > /dev/null; then
-        die "$1 not installed or not in path"
+        die "`$1` not installed or not in path"
     fi
 }
 
-assertCommand "java"
+TMPDIR=`mktemp -d` 
 
-git submodule update --init --recursive
+echo "Installing QuEst++"
+assertCommand "java"
+assertCommand "perl -v"
+assertCommand "unzip"
+git submodule update --init --recursive qe/questplusplus 
+
+echo "Fetching Stanford Core NLP 3.5.1 models"
+if ! `wget -q --show-progress http://nlp.stanford.edu/software/stanford-corenlp-full-2015-01-29.zip -P $TMPDIR` ; then
+    die "Error while downloading"
+fi
+
+unzip "$TMPDIR/stanford-corenlp-full-2015-01-29.zip" "stanford-corenlp-full-2015-01-29/stanford-corenlp-3.5.1-models.jar" -d "$TMPDIR"
+mv "$TMPDIR/stanford-corenlp-full-2015-01-29/stanford-corenlp-3.5.1-models.jar" "qe/questplusplus/lib"
