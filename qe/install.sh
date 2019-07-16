9d49d804592b831346c266ee7708322983fd2a65
Vilém Zouhar <vilem.zouhar@gmail.com>
2019-07-16 09:50:15 +0200

    add spanish model to bash install script

--- qe/install.sh
+++ qe/install.sh
@@ -14,6 +14,14 @@ fi
 unzip -o -q "$TMPDIR/stanford-corenlp-full-2015-01-29.zip" "stanford-corenlp-full-2015-01-29/stanford-corenlp-3.5.1-models.jar" -d "$TMPDIR"
 mv "$TMPDIR/stanford-corenlp-full-2015-01-29/stanford-corenlp-3.5.1-models.jar" "qe/questplusplus/lib"
 
+echo "Fetching Stanford Core Spanish models"
+if ! `wget -q --show-progress "http://nlp.stanford.edu/software/stanford-spanish-corenlp-2015-01-08-models.jar" -P "$TMPDIR"` ; then
+    die "Error while downloading"
+fi
+
+mv "$TMPDIR/stanford-spanish-corenlp-2015-01-08-models.jar" "qe/questplusplus/lib"
+
+
 echo "Fetching Universal WordNet plugin"
 mkdir -p "qe/questplusplus/lang_resources/uwn"
 if ! `wget -q --show-progress "http://resources.mpi-inf.mpg.de/yago-naga/uwn/uwn.zip" -P "$TMPDIR"` ; then
