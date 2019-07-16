c4ca47ece26178fa4fad347a0d00ca9d975a793e
Vilém Zouhar <vilem.zouhar@gmail.com>
2019-07-16 12:52:31 +0200

    move custom configs to a separate dir

--- server/src/qe/questplusplus.py
+++ server/src/qe/questplusplus.py
@@ -16,8 +16,8 @@ class QuestPlusPlus():
                  -alignments lang_resources/alignments/alignments.word-level.out
                  -config ../questplusplus-config/config.word-level.properties
                  """)
-            print(output)
-            print(error)
+            # print(output)
+            # print(error)
             outputFile = 'output/test/output.txt'
             if not os.path.isfile(outputFile):
                 return { 'message': 'Server Processing Error' }
@@ -29,8 +29,11 @@ class QuestPlusPlus():
             os.rmdir('output/test')
             os.rmdir('output')
 
-            # print("Machine Learning")
-            # (output, error) = bash("""
-            #     python src/learn_model.py config/svr.cfg
-            #     """)
+            print("Machine Learning")
+            (output, error) = bash("""
+                python learning/src/learn_model.py ../questplusplus-config/svr.cfg
+                """)
+                
+            print(output)
+            print(error)
             return str(features)
