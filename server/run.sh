45b5b54856d57e92e9720dd376e556178225695f
Vilém Zouhar <vilem.zouhar@gmail.com>
2019-07-19 20:01:04 +0200

    redirect host to local IP address

--- server/run.sh
+++ server/run.sh
@@ -6,7 +6,7 @@ TARGETWD=`realpath "$0" | xargs dirname`
 echo "Changing current working directory to the parent directory of this script (project base dir)"
 cd "$TARGETWD/../"
 
-python3 -m flask run --port 8080
+python3 -m flask run --port 8080 --host 0.0.0.0
 
 echo "Changing current working directory to the previous state"
 cd $PREVWD
