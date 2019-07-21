d184558b35f25c28f45cf2a258e2d1c4d950cc7d
Vilem Zouhar <vilem.zouhar@gmail.com>
2019-07-21 08:50:36 +0000

    remove sudo from inside of nohup script

--- server/run_nohup.sh
+++ server/run_nohup.sh
@@ -3,7 +3,7 @@ TARGETWD=`realpath "$0" | xargs dirname`
 echo "Changing current working directory to the parent directory of this script (project base dir)"
 cd "$TARGETWD/../"
 
-sudo nohup server/run.sh > server/server.log &
+nohup server/run.sh > server/server.log &
 
 echo "Changing current working directory to the previous state"
 cd $PREVWD
