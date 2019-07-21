babdb84cbb3f4385b3ca9b4283ec3e9a1814a504
Vilém Zouhar <vilem.zouhar@gmail.com>
2019-07-21 10:47:58 +0200

    add run_nohup

--- server/install.sh
+++ server/install.sh
@@ -1,3 +1,4 @@
 echo "Installing Ptakopět-server"
+assertCommand "nohup"
 assertPip "flask" 1.0.0
 assertPip "flask-cors" 3.0.0
---
+++ server/run_nohup.sh
@@ -0,0 +1,9 @@
+PREVWD=`pwd`
+TARGETWD=`realpath "$0" | xargs dirname`
+echo "Changing current working directory to the parent directory of this script (project base dir)"
+cd "$TARGETWD/../"
+
+sudo nohup server/run.sh > server/server.log &
+
+echo "Changing current working directory to the previous state"
+cd $PREVWD
