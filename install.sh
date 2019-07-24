710438ed49074e0a1cef5e33f8cec1fd4c5b61bd
Vilem Zouhar <vilem.zouhar@gmail.com>
2019-07-24 12:15:53 +0000

    fix install exports

--- install.sh
+++ install.sh
@@ -50,7 +50,7 @@ assertPip3() {
 export TMPDIR=`mktemp -d` 
 
 
-export -f die assertCommand verlt verlte assertPip
+export -f die assertCommand verlt verlte assertPip2 assertPip3
 
 assertCommand "pip2"
 assertCommand "pip3"
--- server/install.sh
+++ server/install.sh
@@ -6,5 +6,6 @@ assertPip3 "flask" 1.0.0
 assertPip3 "flask-cors" 3.0.0
 
 echo "Allowing the server to run on the machine's IP address and bind to ports <= 1024"
-TARGET_PYTHON3=`readlink -f /bin/python3`
+TARGET_PYTHON3=`which python3`
+TARGET_PYTHON3=`readlink -f ${TARGET_PYTHON3}`
 sudo setcap CAP_NET_BIND_SERVICE=+eip ${TARGET_PYTHON3}
