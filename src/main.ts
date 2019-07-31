5031ea7d7114568349aa8ead74e946b88bbc6bfc
Vilém Zouhar <vilem.zouhar@gmail.com>
2019-07-31 10:37:38 +0200

    Hard https:// redirect.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-// Force HTTPS
-if (location.protocol == 'http:') {
-   location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
-}
+// Force HTTPS using TS
+// if (location.protocol == 'http:') {
+//    location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+// }
 
 import { translator_source, translator_target } from './messages/translator'
 import { SettingsSelector } from './page/settings_selector'
