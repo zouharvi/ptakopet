71be3ed1e551022bbb9ccb46a705db36f3518e32
Vilém Zouhar <vilem.zouhar@gmail.com>
2019-10-08 20:51:26 +0200

    script suite

--- meta/study/log_processing/meta.py
+++ meta/study/log_processing/meta.py
@@ -21,7 +21,7 @@ def userTime(logs, maxrest=60):
         last = x
     return total
 
-def prefixMap(logs, prefix, func):
+def prefixMap(logs, prefix, func = lambda x: x):
     return list(map(func, filter(lambda x: x[0] == prefix, logs)))
 
 def nJoin(l):
@@ -29,4 +29,43 @@ def nJoin(l):
 
 userTime = userTime(logs, 60)
 print(f'User time: {userTime}s, {userTime//60}m, {userTime//(60*60)}h')
-print(nJoin(prefixMap(logs, 'CONFIRM', lambda x: ' ||| '.join(x[3:5]))))
+# print(nJoin(prefixMap(logs, 'CONFIRM', lambda x: ' ||| '.join(x[3:5]))))
+
+def confirmSplit(logs):
+    segments = []
+    curSegment = []
+    for log in logs:
+        if log[0] == 'CONFIRM':
+            segments.append(curSegment)
+            curSegment = []
+        else:
+            curSegment.append(log)
+    return segments
+
+def withoutBacktracking(logs):
+    segments = confirmSplit(logs)
+    out = []
+    for segment in segments:
+        translates = prefixMap(segment, 'TRANSLATE1', lambda x: x[2])
+        prev = ''
+        ok = True
+        for line in translates:
+            if line.startswith(prev):
+                prev = line
+            else:
+                ok = False
+                break
+        if ok:
+            out.append([line, len(segment)])
+    print('Number of all segments:', len(segments))
+    print('Number of segments without backtracking:', len(out))
+
+withoutBacktracking(logs)
+
+def averageTime(logs, maxduration=500):
+    segments = confirmSplit(logs)
+    out = [int(s[-1][1]) - int(s[0][1]) for s in segments]
+    out = list(filter(lambda x: x < maxduration, out))
+    print(f'Average time per task: {sum(out)/len(out):.0f}s')
+
+averageTime(logs)
