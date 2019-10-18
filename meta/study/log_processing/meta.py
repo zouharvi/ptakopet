89f22d63cad90c2063828d30f42e42257741063f
Vilém Zouhar <vilem.zouhar@gmail.com>
2019-10-18 23:07:49 +0200

    work on individual log files

--- meta/study/log_processing/meta.py
+++ meta/study/log_processing/meta.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
-
 import argparse
 
+#
+# Input must be Ptakopět log files each of single person (more precisely
+# the file must be sorted by time tags)
+#
+
 parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('logfile', help='Path to the log file in question')
+parser.add_argument('logfile', nargs='+',
+                    help='Path to the log file in question')
 args = parser.parse_args()
 
-with open(args.logfile, 'r') as f:
-    logs = [x.rstrip('\n').split(',') for x in f.readlines()]
-    logs = [[x.replace('&#44;', ',') for x in line] for line in logs]
 
 def userTime(logs, maxrest=60):
     timestamps = [int(x[1]) for x in logs]
@@ -19,17 +21,14 @@ def userTime(logs, maxrest=60):
         if x - last <= maxrest:
             total += x - last
         last = x
+    print(f'User time: {total}s, {total/60:.1f}m, {total/(60*60):.1f}h')
     return total
 
-def prefixMap(logs, prefix, func = lambda x: x):
+def prefixMap(logs, prefix, func=lambda x: x):
     return list(map(func, filter(lambda x: x[0] == prefix, logs)))
 
 def nJoin(l):
     return '\n'.join(l)
-
-userTime = userTime(logs, 60)
-print(f'User time: {userTime}s, {userTime//60}m, {userTime//(60*60)}h')
-# print(nJoin(prefixMap(logs, 'CONFIRM', lambda x: ' ||| '.join(x[3:5]))))
 
 def confirmSplit(logs):
     segments = []
@@ -47,6 +46,8 @@ def withoutBacktracking(logs):
     out = []
     for segment in segments:
         translates = prefixMap(segment, 'TRANSLATE1', lambda x: x[2])
+        if len(translates) == 0:
+            continue
         prev = ''
         ok = True
         for line in translates:
@@ -59,13 +60,25 @@ def withoutBacktracking(logs):
             out.append([line, len(segment)])
     print('Number of all segments:', len(segments))
     print('Number of segments without backtracking:', len(out))
-
-withoutBacktracking(logs)
+    return out
 
 def averageTime(logs, maxduration=500):
     segments = confirmSplit(logs)
+    segments = filter(lambda x: len(x) > 1, segments)
     out = [int(s[-1][1]) - int(s[0][1]) for s in segments]
     out = list(filter(lambda x: x < maxduration, out))
-    print(f'Average time per task: {sum(out)/len(out):.0f}s')
+    time = sum(out)/len(out)
+    print(f'Average time per task: {time:.0f}s')
+    return time
 
-averageTime(logs)
+# Process every file
+for logfile in args.logfile:
+    with open(logfile, 'r') as f:
+        logs = [x.rstrip('\n').split(',') for x in f.readlines()]
+        logs = [[x.replace('&#44;', ',') for x in line] for line in logs]
+
+    print(f'Processing {logfile}:')
+    userTime(logs, 60)
+    averageTime(logs)
+    # withoutBacktracking(logs)
+    print()
