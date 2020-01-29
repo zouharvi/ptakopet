f0d281a95496c8c2686026adb7b0b72daf811dcd
Vilem Zouhar <vilem.zouhar@gmail.com>
2020-01-29 20:46:16 +0000

    add text3 logging

--- server/src/logger.py
+++ server/src/logger.py
@@ -7,7 +7,7 @@ if __name__ == 'logger':
         print(f'Creating logs dir at {os.getcwd()}/logs')
 
 def organizeData(request):
-    order = ['lang1', 'lang2', 'questionKey', 'confidence', 'reason', 'text1', 'text2', 'estimation', 'alignment', 'queue', 'agent']
+    order = ['lang1', 'lang2', 'questionKey', 'confidence', 'reason', 'text1', 'text2', 'text3', 'estimation', 'alignment', 'queue', 'agent']
     result = []
     for o in order:
         if o in request.keys():
