2e30795bb4fcc4e8d7dbb2fe4fad7888d84a37ab
Vilém Zouhar <vilem.zouhar@gmail.com>
2020-01-29 11:56:48 +0100

    lost in backtranslation

---
+++ meta/study_pilot/processing_scripts/lost_in_bt.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import argparse
+import pickle
+from utils import prefixMap, tokenize
+from difflib import SequenceMatcher
+
+# Lost in Back-Translation export script
+
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('blog2in',  help='Path to input binary log 2 (.blog2) file')
+parser.add_argument('csvout',  help='Path to output tsv file')
+args = parser.parse_args()
+
+with open(args.blog2in, 'rb') as f:
+    segments = pickle.load(f)
+
+def isSimilar(src1, src2):
+    sm = SequenceMatcher(None, tokenize(src1), tokenize(src2))
+    return sm.ratio() > 0.8
+
+out = []
+for segment in segments:
+    if ('final' in segment['rating']) and ('rating' in segment) and ('final' in segment['rating']):
+        text1 = segment['final']['text1']
+        text2 = segment['final']['text2']
+        bts = prefixMap(segment, 'TRANSLATE2')
+        for backObj in bts:
+            if backObj['text2'] == text2:
+                text3 = backObj['text3']
+                if isSimilar(text1, text3):
+                    out.append((segment['rating']['final'], text1, text2, text3))
+                    break
+    # if ('first_viable_src' in segment['rating']) and ('rating' in segment) and ('final' in segment['rating']):
+    #   continue
+        
+# sort by score
+out = sorted(out, key=lambda x: x[0], reverse=True)
+
+with open(args.csvout, 'w') as f:
+    for outObj in out:
+        f.write('\t'.join([str(x) for x in outObj]) + '\n')
