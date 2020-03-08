26dbe189e7d07a5b9b1ea61be17646092d89ddae
Vilém Zouhar <vilem.zouhar@gmail.com>
2020-03-08 12:45:03 +0100

    experiment domain styling

--- meta/study_edin/prepare_questions/domain_messages.py
+++ meta/study_edin/prepare_questions/domain_messages.py
@@ -13,7 +13,7 @@ out = dict()
 with open(args.tsvfile) as tsvfile:
   reader = csv.reader(tsvfile, delimiter='\t')
   for row in reader:
-      out['^' + row[0].rstrip('.png') + '.*'] = {'message': f'Translate the highlighted text in the online form to Czech.<br>Try to achieve the best possible translation quality.<div style="font-weight: bold">({row[1]})</div>'}
+      out['^' + row[0].rstrip('.png') + '.*'] = {'message': f'Translate the highlighted text in the online form to Czech.<br>Try to achieve the best possible translation quality.<div style="margin-top: 15px; font-size: 15pt; font-weight: bold;">{row[1]}</div>'}
 
 if __name__ == '__main__':
     print(json.dumps(out))
