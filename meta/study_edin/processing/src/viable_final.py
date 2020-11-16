ecf6bd71cbcc6309717ec4e6ba623c2575ead83e
Michal Novak <mnovak@ufal.mff.cuni.cz>
2020-11-16 20:22:22 +0100

    new way of calculating the viable-final differences + taking the changetype into account + development printout

--- meta/study_edin/processing/src/viable_final.py
+++ meta/study_edin/processing/src/viable_final.py
@@ -12,27 +12,58 @@ import numpy as np
 from matplotlib.ticker import MaxNLocator
 import re
 import sys
+from pprint import pprint
 
-def extract_viable_scores(data, viable_attr, changetype_filter=None):
+def avg_viable_scores(viables):
     g1 = []
     g2 = []
     g3 = []
     g4 = []
     g5 = []
-    for segment in data:
-        for qalog in getattr(segment, viable_attr):
-            if changetype_filter is not None and hasattr(qalog, "changetype") and re.fullmatch(changetype_filter, qalog.changetype) is None:
-                #print("CHNGTYPE: {:s}".format(qalog.changetype if hasattr(qalog, "changetype") else "None"), file=sys.stderr)
-                continue
-            if changetype_filter is not None:
-                print("CHNGTYPE: '{:s}'".format(qalog.changetype if hasattr(qalog, "changetype") else "None"), file=sys.stderr)
-            g1.append(qalog.src_sti_adq)
-            g2.append(qalog.tgt_src_adq)
-            g3.append(qalog.tgt_sti_adq)
-            g4.append(qalog.tgt_flu)
-            g5.append(qalog.overall)
-    print("G size: {:d}".format(len(g1)), file=sys.stderr)
+    for v in viables:
+        g1.append(v.src_sti_adq)
+        g2.append(v.tgt_src_adq)
+        g3.append(v.tgt_sti_adq)
+        g4.append(v.tgt_flu)
+        g5.append(v.overall)
     return [np.average([x for x in g if x is not None]) for g in [g1, g2, g3, g4, g5]]
+    
+
+def group_by_src(viables):
+    groups = {}
+    for x in viables:
+        if x.src not in groups:
+            groups[x.src] = []
+        groups[x.src].append(x)
+    return groups.values()
+
+def aggregate_viable_final_scores(data):
+    vfc_list = []
+    for segment in data:
+        v_groups = group_by_src(segment.grade_v)
+        f = avg_viable_scores(segment.grade_f)
+        for vg in v_groups:
+            v = avg_viable_scores(vg)
+            c = vg[0].changetype if hasattr(vg[0], "changetype") else None
+            vfc_list.append((v, f, c))
+    return vfc_list
+
+def calculate_plots(vfc):
+    v_matrix = np.array([v for v, f, c in vfc])
+    f_matrix = np.array([f for v, f, c in vfc])
+    diff_matrix = np.array([np.array(f) - np.array(v) for v, f, c in vfc])
+    vgs = np.mean(v_matrix, axis=0)
+    fgs = np.mean(f_matrix, axis=0)
+    dff = np.mean(diff_matrix, axis=0)
+    return vgs, fgs, dff
+
+
+def type_plots(vgs, fgs, dff, header, vfc):
+    print(header)
+    print("C:\t" + str(len(vfc)))
+    print("V:\t" + str(vgs))
+    print("F:\t " + str(fgs))
+    print("F-V:\t " + str(dff))
 
 
 parser = argparse.ArgumentParser(description='Ptakopět log processing.')
@@ -49,17 +80,49 @@ data = [
     not x.invalid
 ]
 
-vgs = extract_viable_scores(data, "grade_v")
-fgs = extract_viable_scores(data, "grade_f")
-cvgs = extract_viable_scores(data, "grade_v", r"^[^X]*$")
-dff = [y-x for (x,y) in zip(vgs, fgs)]
+vfc = aggregate_viable_final_scores(data)
+vgs, fgs, dff = calculate_plots(vfc)
+type_plots(vgs, fgs, dff, "========= All viable-final pairs ========", vfc)
+
+vfc = [vfc_i for vfc_i in vfc if vfc_i[2] is not None]
+vgs, fgs, dff = calculate_plots(vfc)
+type_plots(vgs, fgs, dff, "========= * viable<>final pairs ========", vfc)
+
+vfc_X = [vfc_i for vfc_i in vfc if re.match(r"X", vfc_i[2])]
+vgs, fgs, dff = calculate_plots(vfc_X)
+type_plots(vgs, fgs, dff, "========= * X pairs ========", vfc_X)
+
+vfc_PM = [vfc_i for vfc_i in vfc if re.match(r"PM", vfc_i[2])]
+vgs, fgs, dff = calculate_plots(vfc_PM)
+type_plots(vgs, fgs, dff, "========= * PM pairs ========", vfc_PM)
+
+vfc_noXPM = [vfc_i for vfc_i in vfc if not re.match(r"X", vfc_i[2]) and not re.match(r"PM", vfc_i[2])]
+vgs, fgs, dff = calculate_plots(vfc_noXPM)
+type_plots(vgs, fgs, dff, "========= * not (X or PM) pairs ========", vfc_noXPM)
+
+vfc_P = [vfc_i for vfc_i in vfc_noXPM if re.match(r"P", vfc_i[2])]
+vgs, fgs, dff = calculate_plots(vfc_P)
+type_plots(vgs, fgs, dff, "========= * P pairs ========", vfc_P)
+
+vfc_PG = [vfc_i for vfc_i in vfc_noXPM if re.match(r"PG", vfc_i[2])]
+vgs, fgs, dff = calculate_plots(vfc_PG)
+type_plots(vgs, fgs, dff, "========= * PG pairs ========", vfc_PG)
+
+vfc_PnotG = [vfc_i for vfc_i in vfc_P if not re.match(r"PG", vfc_i[2])]
+vgs, fgs, dff = calculate_plots(vfc_PnotG)
+type_plots(vgs, fgs, dff, "========= * P-PG pairs ========", vfc_PnotG)
+
+vfc_Q = [vfc_i for vfc_i in vfc_noXPM if re.match(r"Q", vfc_i[2])]
+vgs, fgs, dff = calculate_plots(vfc_Q)
+type_plots(vgs, fgs, dff, "========= * Q pairs ========", vfc_Q)
+
+# exit()
 
 OFFSETX = 0.15
 fig = plt.figure(figsize=(4.2, 3.2))
 ax1 = fig.add_subplot()
 vh = ax1.plot(range(5), vgs, markersize=8, marker='s', color='darkblue', alpha=0.6)
 fh = ax1.plot(range(5), fgs, markersize=8, marker='v', color='darkgreen', alpha=0.6)
-cvh = ax1.plot(range(5), cvgs, markersize=8, marker='o', color='darkorange', alpha=0.6)
 ax1.set_ylim(4, 4.7)
 ax1.set_xticks(range(5))
 ax1.set_xticklabels(['SRC-STI', 'TGT-SRC', 'TGT-STI', 'Fluency', 'Overall'])
@@ -71,8 +134,8 @@ ax2.set_ylim(0, 0.15)
 ax2.set_ylabel('Difference')
 
 plt.legend(
-    vh+fh+cvh+dh,
-    ['Viables', 'Final', 'Correct viables', 'Final-Viables'],
+    vh+fh+dh,
+    ['Viables', 'Final', 'Final-Viables'],
     loc='upper right',
     ncol=1,
 )
