1e6624ed02a7c4b1918ad29df04ce50633ac593c
Michal Novak <mnovak@ufal.mff.cuni.cz>
2020-11-16 14:57:43 +0100

    show scores for correct viables + output to pdf

--- meta/study_edin/processing/src/viable_final.py
+++ meta/study_edin/processing/src/viable_final.py
@@ -10,9 +10,34 @@ from functools import reduce
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.ticker import MaxNLocator
+import re
+import sys
+
+def extract_viable_scores(data, viable_attr, changetype_filter=None):
+    g1 = []
+    g2 = []
+    g3 = []
+    g4 = []
+    g5 = []
+    for segment in data:
+        for qalog in getattr(segment, viable_attr):
+            if changetype_filter is not None and hasattr(qalog, "changetype") and re.fullmatch(changetype_filter, qalog.changetype) is None:
+                #print("CHNGTYPE: {:s}".format(qalog.changetype if hasattr(qalog, "changetype") else "None"), file=sys.stderr)
+                continue
+            if changetype_filter is not None:
+                print("CHNGTYPE: '{:s}'".format(qalog.changetype if hasattr(qalog, "changetype") else "None"), file=sys.stderr)
+            g1.append(qalog.src_sti_adq)
+            g2.append(qalog.tgt_src_adq)
+            g3.append(qalog.tgt_sti_adq)
+            g4.append(qalog.tgt_flu)
+            g5.append(qalog.overall)
+    print("G size: {:d}".format(len(g1)), file=sys.stderr)
+    return [np.average([x for x in g if x is not None]) for g in [g1, g2, g3, g4, g5]]
+
 
 parser = argparse.ArgumentParser(description='Ptakopět log processing.')
 parser.add_argument('blog3', help='Path to a blog3 file')
+parser.add_argument('--pdf', type=str, help='Instead of showing, print to the specified PDF file')
 args = parser.parse_args()
 
 with open(args.blog3, 'rb') as f:
@@ -24,44 +49,9 @@ data = [
     not x.invalid
 ]
 
-vg1 = []
-vg2 = []
-vg3 = []
-vg4 = []
-vg5 = []
-fg1 = []
-fg2 = []
-fg3 = []
-fg4 = []
-fg5 = []
-
-for segment in data:
-    for qalog in segment.grade_v:
-        vg1.append(qalog.src_sti_adq)
-        vg2.append(qalog.tgt_src_adq)
-        vg3.append(qalog.tgt_sti_adq)
-        vg4.append(qalog.tgt_flu)
-        vg5.append(qalog.overall)
-    for qalog in segment.grade_f:
-        fg1.append(qalog.src_sti_adq)
-        fg2.append(qalog.tgt_src_adq)
-        fg3.append(qalog.tgt_sti_adq)
-        fg4.append(qalog.tgt_flu)
-        fg5.append(qalog.overall)
-
-vg1 = np.average([x for x in vg1 if x is not None])
-vg2 = np.average([x for x in vg2 if x is not None])
-vg3 = np.average([x for x in vg3 if x is not None])
-vg4 = np.average([x for x in vg4 if x is not None])
-vg5 = np.average([x for x in vg5 if x is not None])
-fg1 = np.average([x for x in fg1 if x is not None])
-fg2 = np.average([x for x in fg2 if x is not None])
-fg3 = np.average([x for x in fg3 if x is not None])
-fg4 = np.average([x for x in fg4 if x is not None])
-fg5 = np.average([x for x in fg5 if x is not None])
-
-vgs = [vg1, vg2, vg3, vg4, vg5]
-fgs = [fg1, fg2, fg3, fg4, fg5]
+vgs = extract_viable_scores(data, "grade_v")
+fgs = extract_viable_scores(data, "grade_f")
+cvgs = extract_viable_scores(data, "grade_v", r"^[^X]*$")
 dff = [y-x for (x,y) in zip(vgs, fgs)]
 
 OFFSETX = 0.15
@@ -69,6 +59,7 @@ fig = plt.figure(figsize=(4.2, 3.2))
 ax1 = fig.add_subplot()
 vh = ax1.plot(range(5), vgs, markersize=8, marker='s', color='darkblue', alpha=0.6)
 fh = ax1.plot(range(5), fgs, markersize=8, marker='v', color='darkgreen', alpha=0.6)
+cvh = ax1.plot(range(5), cvgs, markersize=8, marker='o', color='darkorange', alpha=0.6)
 ax1.set_ylim(4, 4.7)
 ax1.set_xticks(range(5))
 ax1.set_xticklabels(['SRC-STI', 'TGT-SRC', 'TGT-STI', 'Fluency', 'Overall'])
@@ -80,10 +71,13 @@ ax2.set_ylim(0, 0.15)
 ax2.set_ylabel('Difference')
 
 plt.legend(
-    vh+fh+dh,
-    ['Viables', 'Final', 'Final-Viables'],
+    vh+fh+cvh+dh,
+    ['Viables', 'Final', 'Correct viables', 'Final-Viables'],
     loc='upper right',
     ncol=1,
 )
 fig.tight_layout(rect=[-0.03, -0.02, 1.01, 1.03])
 plt.show()
+
+if args.pdf is not None:
+    fig.savefig(args.pdf)
