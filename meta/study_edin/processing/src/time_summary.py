c9dd57f857575fdcbf1609cf01c45a140b21c66e
Michal Novak <mnovak@ufal.mff.cuni.cz>
2020-11-18 14:53:49 +0100

    average time -> median time + median time_per_action added

--- meta/study_edin/processing/src/time_summary.py
+++ meta/study_edin/processing/src/time_summary.py
@@ -21,6 +21,9 @@ total_time = []
 config_events = {}
 lang_events = {}
 total_events = []
+config_timeperevent = {}
+lang_timeperevent = {}
+total_timeperevent = []
 
 def segment_time(segment):
     times = [int(x[1]) for x in segment.data]
@@ -29,9 +32,14 @@ def segment_time(segment):
 def segment_events(segment):
     return sum([1 for x in segment.data if x[0] in {'TRANSLATE1', 'TRANSLATE2'}])/2
 
-def average_sane(data, limitL=0, limitR=60*10):
+def average_sane(data, limitL=0, limitR=60*10, method=np.average):
     l = [x for x in data if x <= limitR and x >= limitL]
-    return np.average(l)
+    return method(l)
+
+def average_sane_timeperevent(timeperevents, events, limitL=0, limitR=60*10, method=np.median):
+    tpee = zip(timeperevents, events)
+    l = [tpe for tpe, e in tpee if e <= limitR and e >= limitL]
+    return method(l)
 
 for segment in data:
     s_time = segment_time(segment)
@@ -39,27 +47,35 @@ for segment in data:
     lang_time.setdefault(segment.cid.engine,[]).append(s_time)
     total_time.append(s_time)
 
-
     s_events = segment_events(segment)
     config_events.setdefault(segment.cid.nicename_nomt_noft(), []).append(s_events)
     lang_events.setdefault(segment.cid.engine,[]).append(s_events)
     total_events.append(s_events)
+    
+    s_timeperevent = s_time / s_events if s_events != 0 else None
+    config_timeperevent.setdefault(segment.cid.nicename_nomt_noft(), []).append(s_timeperevent)
+    lang_timeperevent.setdefault(segment.cid.engine,[]).append(s_timeperevent)
+    total_timeperevent.append(s_timeperevent)
 
 print(('\n'+'%'*10)*4)
 
-print(f'Czech MT 1 & {average_sane(lang_time["csw"]):.0f} & {average_sane(lang_events["csw"], limitL=1, limitR=1000):.2f}  \\\\') 
-print(f'Czech MT 2 & {average_sane(lang_time["cs"]):.0f}  & {average_sane(lang_events["cs"],  limitL=1, limitR=1000):.2f}  \\\\') 
-print(f'Czech MT 3 & {average_sane(lang_time["css"]):.0f} & {average_sane(lang_events["css"], limitL=1, limitR=1000):.2f}  \\\\') 
-print(f'Estonian   & {average_sane(lang_time["et"]):.0f}  & {average_sane(lang_events["et"],  limitL=1, limitR=1000):.2f}  \\\\ \hline') 
+#csw_zip = list(zip(lang_time["csw"], lang_events["csw"], lang_timeperevent["csw"]))
+#print(csw_zip)
+
+print(f'Czech MT 1 & {average_sane(lang_time["csw"], method=np.median):.0f} & {average_sane(lang_events["csw"], limitL=1, limitR=1000):.2f} & {average_sane_timeperevent(lang_timeperevent["csw"], lang_events["csw"], limitL=1, limitR=1000):.0f} \\\\') 
+print(f'Czech MT 2 & {average_sane(lang_time["cs"], method=np.median):.0f}  & {average_sane(lang_events["cs"],  limitL=1, limitR=1000):.2f} & {average_sane_timeperevent(lang_timeperevent["cs"], lang_events["cs"], limitL=1, limitR=1000):.0f} \\\\') 
+print(f'Czech MT 3 & {average_sane(lang_time["css"], method=np.median):.0f} & {average_sane(lang_events["css"], limitL=1, limitR=1000):.2f} & {average_sane_timeperevent(lang_timeperevent["css"], lang_events["css"], limitL=1, limitR=1000):.0f} \\\\') 
+print(f'Estonian   & {average_sane(lang_time["et"], method=np.median):.0f}  & {average_sane(lang_events["et"],  limitL=1, limitR=1000):.2f} & {average_sane_timeperevent(lang_timeperevent["et"], lang_events["et"], limitL=1, limitR=1000):.0f} \\\\ \hline') 
 
 for order in CONFIG_ORDER:
     cid = CID(order)
-    time = average_sane(config_time[cid.nicename_nomt_noft()])
+    time = average_sane(config_time[cid.nicename_nomt_noft()], method=np.median)
     events = average_sane(config_events[cid.nicename_nomt_noft()], limitL=1, limitR=1000)
-    print(f'{cid.nicename_nomt_noft()} & {time:.0f} & {events:.2f} \\\\')
+    timeperevent = average_sane_timeperevent(config_timeperevent[cid.nicename_nomt_noft()], config_events[cid.nicename_nomt_noft()], limitL=1, limitR=1000)
+    print(f'{cid.nicename_nomt_noft()} & {time:.0f} & {events:.2f} & {timeperevent:.0f} \\\\')
 
 print('\\hline')
 
-print(f'Total & {average_sane(total_time):.0f} & {average_sane(total_events, limitL=1, limitR=1000):.2f} \\\\')
+print(f'Total & {average_sane(total_time, method=np.median):.0f} & {average_sane(total_events, limitL=1, limitR=1000):.2f} & {average_sane_timeperevent(total_timeperevent, total_events, limitL=1, limitR=1000):.0f} \\\\')
 
 print(('\n'+'%'*10)*4)
