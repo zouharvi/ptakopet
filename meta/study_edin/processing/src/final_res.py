c0de57de50ee7f60a88f2ccf7cfb6efba266caf9
Michal Novak <mnovak@ufal.mff.cuni.cz>
2020-11-20 10:47:01 +0100

    commented data limitation of those with multiple viables

--- meta/study_edin/processing/src/final_res.py
+++ meta/study_edin/processing/src/final_res.py
@@ -39,6 +39,9 @@ with open(args.blog3, 'rb') as f:
 # Take only successful ones
 data = [x for x in data if (not x.invalid) and (len(x.grade_f) != 0)]
 
+#data = [x for x in data if len(x.grade_v) > 0]
+#data = [x for x in data if any([x.grade_f[0].src != g.src for g in x.grade_v])]
+
 data_line = filter_data(data, lambda sid: sid.engine == "csw")
 scores_line = average_final_scores(data_line)
 print_tab_line(scores_line, "Czech MT 1")
