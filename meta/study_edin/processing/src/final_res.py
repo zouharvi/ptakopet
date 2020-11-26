5ba0b2934aabc75f6e9b7d9f5f0096d68255e792
Michal Novak <mnovak@ufal.mff.cuni.cz>
2020-11-26 23:30:41 +0100

    calculate statistical significance for the final results table and viable diff table

--- meta/study_edin/processing/src/final_res.py
+++ meta/study_edin/processing/src/final_res.py
@@ -6,22 +6,51 @@ from load import Segment, CID
 from grades import QALog, aggr_grades_by_src
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import mannwhitneyu
 import sys
 
 def filter_data(data, config_filter=None):
     return [ x for x in data if config_filter(x.cid) ]
 
-def average_final_scores(data):
+def average_final_scores(data, return_grades=False, return_confid=False):
     all_grades = np.array([aggr_grades_by_src(x.grade_f)[0][2] for x in data], dtype=float)
     all_confid = np.array([x.score for x in data], dtype=float)
     avg_grades = np.nanmean(all_grades, axis=0)
     avg_confid = np.nanmean(all_confid, axis=0)
-    return avg_grades.tolist() + [ avg_confid.tolist() ]
+    avg = avg_grades.tolist() + [ avg_confid.tolist() ]
+    return_tuple = tuple()
+    if return_grades:
+        return_tuple += (all_grades,)
+    if return_confid:
+        return_tuple += (all_confid,)
+    if len(return_tuple) > 0:
+        return (avg,) + return_tuple
+    else:
+        return avg
 
-def print_tab_line(scores, title):
-    linestr = " & ".join([title] + ["{:.2f}".format(s) for s in scores])
+def calculate_pvalues(grades1, grades2, confids1, confids2):
+    pvalues = []
+    for i in range(grades1.shape[1]):
+        (u, pvalue) = mannwhitneyu(grades1[:,i], grades2[:,i])
+        pvalues.append(pvalue)
+    (u, pvalue) = mannwhitneyu(confids1, confids2)
+    pvalues.append(pvalue)
+    return pvalues
+
+def print_tab_line(scores, title, pvalues=None):
+    linestr = " & ".join([title] + ["{:.2f}{:s}".format(s, significance_symbol(pvalues[i]) if pvalues is not None else "") for i, s in enumerate(scores)])
     linestr += " \\\\"
     print(linestr)
+
+def significance_symbol(pvalue):
+    if pvalue < 0.001:
+        return "\\threeS"
+    elif pvalue < 0.01:
+        return "\\twoS"
+    elif pvalue < 0.05:
+        return "\\oneS"
+    else:
+        return ""
 
 parser = argparse.ArgumentParser(description='PBML log processing.')
 parser.add_argument('blog3', help='Path to a blog3 file')
@@ -58,43 +87,58 @@ print("Estonian: len = {:d}".format(len(data_line)), file=sys.stderr)
 
 print("\\midrule")
 
+data_line = filter_data(data, lambda sid: not sid.bt and not sid.qe and not sid.pp)
+(scores_nocue, grades_nocue, confid_nocue) = average_final_scores(data_line, return_grades=True, return_confid=True)
+
 data_line = filter_data(data, lambda sid: sid.bt and sid.qe and sid.pp)
-scores_line = average_final_scores(data_line)
-print_tab_line(scores_line, "BT QE PP")
+(scores_line, grades_line, confid_line) = average_final_scores(data_line, return_grades=True, return_confid=True)
+pvalues_line = calculate_pvalues(grades_line, grades_nocue, confid_line, confid_nocue)
+print_tab_line(scores_line, "BT QE PP", pvalues_line)
 print("BT QE PP: len = {:d}".format(len(data_line)), file=sys.stderr)
+print(pvalues_line, file=sys.stderr)
 
 data_line = filter_data(data, lambda sid: sid.bt and sid.qe and not sid.pp)
-scores_line = average_final_scores(data_line)
-print_tab_line(scores_line, "BT QE")
+(scores_line, grades_line, confid_line) = average_final_scores(data_line, return_grades=True, return_confid=True)
+pvalues_line = calculate_pvalues(grades_line, grades_nocue, confid_line, confid_nocue)
+print_tab_line(scores_line, "BT QE", pvalues_line)
 print("BT QE: len = {:d}".format(len(data_line)), file=sys.stderr)
+print(pvalues_line, file=sys.stderr)
 
 data_line = filter_data(data, lambda sid: sid.bt and not sid.qe and sid.pp)
-scores_line = average_final_scores(data_line)
-print_tab_line(scores_line, "BT PP")
+(scores_line, grades_line, confid_line) = average_final_scores(data_line, return_grades=True, return_confid=True)
+pvalues_line = calculate_pvalues(grades_line, grades_nocue, confid_line, confid_nocue)
+print_tab_line(scores_line, "BT PP", pvalues_line)
 print("BT PP: len = {:d}".format(len(data_line)), file=sys.stderr)
+print(pvalues_line, file=sys.stderr)
 
 data_line = filter_data(data, lambda sid: not sid.bt and sid.qe and sid.pp)
-scores_line = average_final_scores(data_line)
-print_tab_line(scores_line, "QE PP")
+(scores_line, grades_line, confid_line) = average_final_scores(data_line, return_grades=True, return_confid=True)
+pvalues_line = calculate_pvalues(grades_line, grades_nocue, confid_line, confid_nocue)
+print_tab_line(scores_line, "QE PP", pvalues_line)
 print("QE PP: len = {:d}".format(len(data_line)), file=sys.stderr)
+print(pvalues_line, file=sys.stderr)
 
 data_line = filter_data(data, lambda sid: sid.bt and not sid.qe and not sid.pp)
-scores_line = average_final_scores(data_line)
-print_tab_line(scores_line, "BT")
+(scores_line, grades_line, confid_line) = average_final_scores(data_line, return_grades=True, return_confid=True)
+pvalues_line = calculate_pvalues(grades_line, grades_nocue, confid_line, confid_nocue)
+print_tab_line(scores_line, "BT", pvalues_line)
 print("BT: len = {:d}".format(len(data_line)), file=sys.stderr)
+print(pvalues_line, file=sys.stderr)
 
 data_line = filter_data(data, lambda sid: not sid.bt and sid.qe and not sid.pp)
-scores_line = average_final_scores(data_line)
-print_tab_line(scores_line, "QE")
+(scores_line, grades_line, confid_line) = average_final_scores(data_line, return_grades=True, return_confid=True)
+pvalues_line = calculate_pvalues(grades_line, grades_nocue, confid_line, confid_nocue)
+print_tab_line(scores_line, "QE", pvalues_line)
 print("QE: len = {:d}".format(len(data_line)), file=sys.stderr)
+print(pvalues_line, file=sys.stderr)
 
 data_line = filter_data(data, lambda sid: not sid.bt and not sid.qe and sid.pp)
-scores_line = average_final_scores(data_line)
-print_tab_line(scores_line, "PP")
+(scores_line, grades_line, confid_line) = average_final_scores(data_line, return_grades=True, return_confid=True)
+pvalues_line = calculate_pvalues(grades_line, grades_nocue, confid_line, confid_nocue)
+print_tab_line(scores_line, "PP", pvalues_line)
 print("PP: len = {:d}".format(len(data_line)), file=sys.stderr)
+print(pvalues_line, file=sys.stderr)
 
-data_line = filter_data(data, lambda sid: not sid.bt and not sid.qe and not sid.pp)
-scores_line = average_final_scores(data_line)
 print_tab_line(scores_line, "--")
 print("--: len = {:d}".format(len(data_line)), file=sys.stderr)
 
--- meta/study_edin/processing/src/viable_final_diff.py
+++ meta/study_edin/processing/src/viable_final_diff.py
@@ -7,26 +7,54 @@ from grades import QALog, aggr_grades_by_src
 import numpy as np
 import re
 import sys
+from scipy.stats import wilcoxon
 
 def filter_data(data, config_filter=None):
     return [ x for x in data if config_filter(x.cid) ]
 
-def average_grade_diffs(data):
+def average_grade_diffs(data, return_diffs=True):
     grade_diffs = []
     for x in data:
         f = aggr_grades_by_src(x.grade_f)[0]
         v_list = aggr_grades_by_src(x.grade_v, changetype=True)
         v_list = [ v for v in v_list if v[0] != f[0] ]
-        v_list = [ v for v in v_list if not re.match(r'(X|PM)', v[3]) ]
-        diff_list = [ f[2] - v[2] for v in v_list ]
-        grade_diffs.extend(diff_list)
+        v_list = [ v for v in v_list if not re.match(r'X', v[3]) ]
+        if len(v_list) > 0:
+            diff_list = [ f[2] - v[2] for v in v_list ]
+            grade_diffs.extend(diff_list)
+            #avg_diff = np.nanmean(np.array(diff_list, dtype=float), axis=0)
+            #grade_diffs.append(avg_diff)
     avg_grades = np.nanmean(np.array(grade_diffs, dtype=float), axis=0)
-    return avg_grades.tolist()
+    if return_diffs:
+        return (avg_grades.tolist(), np.array(grade_diffs, dtype=float))
+    else:
+        return avg_grades.tolist()
 
-def print_tab_line(scores, title):
-    linestr = " & ".join([title] + ["\\textcolor{{{:s}}}{{{:+.2f}}}".format("darkgreen" if df_val > 0 else "darkred", df_val) for df_val in scores])
+def calculate_pvalues(grade_diffs):
+    pvalues = []
+    for i in range(grade_diffs.shape[1]):
+        (stat, pvalue) = wilcoxon(grade_diffs[:,i])
+        pvalues.append(pvalue)
+    return pvalues
+
+def print_tab_line(scores, title, pvalues):
+    linestr = " & ".join([title] + ["\\textcolor{{{:s}}}{{{:+.2f}}}{:s}".format(
+        "darkgreen" if df_val > 0 else "darkred",
+        df_val,
+        significance_symbol(pvalues[i]) if pvalues is not None else ""
+        ) for i, df_val in enumerate(scores)])
     linestr += " \\\\"
     print(linestr)
+
+def significance_symbol(pvalue):
+    if pvalue < 0.001:
+        return "\\threeS"
+    elif pvalue < 0.01:
+        return "\\twoS"
+    elif pvalue < 0.05:
+        return "\\oneS"
+    else:
+        return ""
 
 parser = argparse.ArgumentParser(description='Generate a table of score differences between intermediate and final viable inputs.')
 parser.add_argument('blog3', help='Path to a blog3 file')
@@ -44,70 +72,88 @@ data = [
 data = [x for x in data if len(x.grade_v) > 0]
 data = [x for x in data if any([x.grade_f[0].src != g.src for g in x.grade_v])]
 
-data_line = filter_data(data, lambda sid: sid.engine == "csw")
-scores_line = average_grade_diffs(data_line)
-print_tab_line(scores_line, "Czech 1")
-print("Czech 1: len = {:d}".format(len(data_line)), file=sys.stderr)
-
-data_line = filter_data(data, lambda sid: sid.engine == "cs")
-scores_line = average_grade_diffs(data_line)
-print_tab_line(scores_line, "Czech 2")
-print("Czech 2: len = {:d}".format(len(data_line)), file=sys.stderr)
-
-data_line = filter_data(data, lambda sid: sid.engine == "css")
-scores_line = average_grade_diffs(data_line)
-print_tab_line(scores_line, "Czech 3")
-print("Czech 3: len = {:d}".format(len(data_line)), file=sys.stderr)
-
-data_line = filter_data(data, lambda sid: sid.engine == "et")
-scores_line = average_grade_diffs(data_line)
-print_tab_line(scores_line, "Estonian")
-print("Estonian: len = {:d}".format(len(data_line)), file=sys.stderr)
-
-print("\\midrule")
+#data_line = filter_data(data, lambda sid: sid.engine == "csw")
+#scores_line = average_grade_diffs(data_line)
+#print_tab_line(scores_line, "Czech 1")
+#print("Czech 1: len = {:d}".format(len(data_line)), file=sys.stderr)
+#
+#data_line = filter_data(data, lambda sid: sid.engine == "cs")
+#scores_line = average_grade_diffs(data_line)
+#print_tab_line(scores_line, "Czech 2")
+#print("Czech 2: len = {:d}".format(len(data_line)), file=sys.stderr)
+#
+#data_line = filter_data(data, lambda sid: sid.engine == "css")
+#scores_line = average_grade_diffs(data_line)
+#print_tab_line(scores_line, "Czech 3")
+#print("Czech 3: len = {:d}".format(len(data_line)), file=sys.stderr)
+#
+#data_line = filter_data(data, lambda sid: sid.engine == "et")
+#scores_line = average_grade_diffs(data_line)
+#print_tab_line(scores_line, "Estonian")
+#print("Estonian: len = {:d}".format(len(data_line)), file=sys.stderr)
+#
+#print("\\midrule")
 
 data_line = filter_data(data, lambda sid: sid.bt and sid.qe and sid.pp)
-scores_line = average_grade_diffs(data_line)
-print_tab_line(scores_line, "BT QE PP")
+(scores_line, diffs_line) = average_grade_diffs(data_line)
+pvalues_line = calculate_pvalues(diffs_line)
+print_tab_line(scores_line, "BT QE PP", pvalues_line)
 print("BT QE PP: len = {:d}".format(len(data_line)), file=sys.stderr)
+print(pvalues_line, file=sys.stderr)
 
 data_line = filter_data(data, lambda sid: sid.bt and sid.qe and not sid.pp)
-scores_line = average_grade_diffs(data_line)
-print_tab_line(scores_line, "BT QE")
+(scores_line, diffs_line) = average_grade_diffs(data_line)
+pvalues_line = calculate_pvalues(diffs_line)
+print_tab_line(scores_line, "BT QE", pvalues_line)
 print("BT QE: len = {:d}".format(len(data_line)), file=sys.stderr)
+print(pvalues_line, file=sys.stderr)
 
 data_line = filter_data(data, lambda sid: sid.bt and not sid.qe and sid.pp)
-scores_line = average_grade_diffs(data_line)
-print_tab_line(scores_line, "BT PP")
+(scores_line, diffs_line) = average_grade_diffs(data_line)
+pvalues_line = calculate_pvalues(diffs_line)
+print_tab_line(scores_line, "BT PP", pvalues_line)
 print("BT PP: len = {:d}".format(len(data_line)), file=sys.stderr)
+print(pvalues_line, file=sys.stderr)
 
 data_line = filter_data(data, lambda sid: not sid.bt and sid.qe and sid.pp)
-scores_line = average_grade_diffs(data_line)
-print_tab_line(scores_line, "QE PP")
+(scores_line, diffs_line) = average_grade_diffs(data_line)
+pvalues_line = calculate_pvalues(diffs_line)
+print_tab_line(scores_line, "QE PP", pvalues_line)
 print("QE PP: len = {:d}".format(len(data_line)), file=sys.stderr)
+print(pvalues_line, file=sys.stderr)
 
 data_line = filter_data(data, lambda sid: sid.bt and not sid.qe and not sid.pp)
-scores_line = average_grade_diffs(data_line)
-print_tab_line(scores_line, "BT")
+(scores_line, diffs_line) = average_grade_diffs(data_line)
+pvalues_line = calculate_pvalues(diffs_line)
+print_tab_line(scores_line, "BT", pvalues_line)
 print("BT: len = {:d}".format(len(data_line)), file=sys.stderr)
+print(pvalues_line, file=sys.stderr)
 
 data_line = filter_data(data, lambda sid: not sid.bt and sid.qe and not sid.pp)
-scores_line = average_grade_diffs(data_line)
-print_tab_line(scores_line, "QE")
+(scores_line, diffs_line) = average_grade_diffs(data_line)
+pvalues_line = calculate_pvalues(diffs_line)
+print_tab_line(scores_line, "QE", pvalues_line)
 print("QE: len = {:d}".format(len(data_line)), file=sys.stderr)
+print(pvalues_line, file=sys.stderr)
 
 data_line = filter_data(data, lambda sid: not sid.bt and not sid.qe and sid.pp)
-scores_line = average_grade_diffs(data_line)
-print_tab_line(scores_line, "PP")
+(scores_line, diffs_line) = average_grade_diffs(data_line)
+pvalues_line = calculate_pvalues(diffs_line)
+print_tab_line(scores_line, "PP", pvalues_line)
 print("PP: len = {:d}".format(len(data_line)), file=sys.stderr)
+print(pvalues_line, file=sys.stderr)
 
 data_line = filter_data(data, lambda sid: not sid.bt and not sid.qe and not sid.pp)
-scores_line = average_grade_diffs(data_line)
-print_tab_line(scores_line, "--")
+(scores_line, diffs_line) = average_grade_diffs(data_line)
+pvalues_line = calculate_pvalues(diffs_line)
+print_tab_line(scores_line, "--", pvalues_line)
 print("--: len = {:d}".format(len(data_line)), file=sys.stderr)
+print(pvalues_line, file=sys.stderr)
 
 print("\\midrule")
 
-scores_line = average_grade_diffs(data)
-print_tab_line(scores_line, "Total")
+(scores_line, diffs_line) = average_grade_diffs(data)
+pvalues_line = calculate_pvalues(diffs_line)
+print_tab_line(scores_line, "Total", pvalues_line)
 print("TOTAL: len = {:d}".format(len(data)), file=sys.stderr)
+print(pvalues_line, file=sys.stderr)
